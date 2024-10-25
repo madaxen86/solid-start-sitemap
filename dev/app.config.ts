c8aa32027c1994b473f61e573c3ebe7aaf039511
madaxen86 <59310516+madaxen86@users.noreply.github.com>
2024-10-25 17:03:51 +0200

    init setup

---
+++ dev/app.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from '@solidjs/start/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+import Plugin from '../src/plugin';
+
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@src': path.resolve(__dirname, '../src'),
+        '@plugin': path.resolve(__dirname, '../src/plugin'),
+      },
+    },
+    plugins: [
+      Plugin({
+        hostname: 'http://localhost:3000',
+        replaceRouteParams: {
+          ':postId': [1, 2, 3],
+        },
+        limit: 5000,
+      }),
+    ],
+    optimizeDeps: {
+      include: ['unenv'],
+    },
+  },
+  server: {
+    experimental: {
+      openAPI: true,
+    },
+    alias: {
+      consola: 'consola',
+    },
+  },
+});
